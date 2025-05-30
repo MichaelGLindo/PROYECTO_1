dd344dc00a5d4871cd1f276188b1d0b100f7f2ec
MichaelGLindo <maicolflass10@gmail.com>
2025-05-30 15:58:55 -0500

    subiendo el proyecto

---
+++ vistas/empresa/iniciar_sesion.vista.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Empresa</title>
+</head>
+<body>
+    <h1>Formulario Iniciar Sesión</h1>
+    <a href="?">Cancelar</a>
+</body>
+
+</html>
